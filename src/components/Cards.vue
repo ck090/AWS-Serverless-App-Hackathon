b38bb39fc0eb61d914363e6a50670bcc0f542846
ck090 <chandrakanth@Chandras-MacBook-Pro.local>
2020-08-23 01:43:43 +0530

    First UI changes

---
+++ src/components/Cards.vue
@@ -0,0 +1,95 @@
+<template>
+  <div class="hello">
+    <h1>Welcome back {{ msg }}</h1>
+    <h2>Your Feed:</h2>
+    <div id="cards">
+        <div id="c1">
+            <div id="content">
+                <strong>Vincent Lombadri</strong>
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+            </div>
+        </div>
+        <div id="c1">
+            <div id="content">
+                <strong>Vincent Lombadri</strong>
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+            </div>
+        </div>
+        <div id="c1">
+            <div id="content">
+                <strong>Vincent Lombadri</strong>
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+            </div>
+        </div>
+        <div id="c1">
+            <div id="content">
+                <strong>Vincent Lombadri</strong>
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+            </div>
+        </div>
+        <div id="c1">
+            <div id="content">
+                <strong>Vincent Lombadri</strong>
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+            </div>
+        </div>
+        <div id="c1">
+            <div id="content">
+                <strong>Vincent Lombadri</strong>
+            </div>
+        </div>
+        <div id="c1">
+            <div id="content">
+                <strong>Vincent Lombadri</strong>
+            </div>
+        </div>
+        <div id="c1">
+            <div id="content">
+                <strong>Vincent Lombadri</strong>
+                using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+            </div>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+#cards {
+    display: flexbox;
+    flex-direction: row;
+}
+
+#c1 {
+    background: rgb(29, 48, 73);
+    color: white;
+    border-radius: 1rem;
+    margin: 1rem;
+    margin-right: 5rem;
+    margin-left: 5rem;
+    box-shadow: 0px 0px 20px 0px rgb(0, 0, 0);
+}
+
+#content {
+    padding: 2rem;
+    margin: 1rem;
+}
+
+.hello {
+    -webkit-animation: fadein 2s;
+}
+
+@-webkit-keyframes fadein {
+    from { opacity: 0; }
+    to   { opacity: 1; }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,67 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="nav">
+      <router-link to="/">Home</router-link><span id="title">HuMan United</span>
+      <router-link to="/about">About</router-link>
+    </div>
+    <Cards msg="N Chandra Kanth"/>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Cards from '@/components/Cards.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    Cards
   }
 }
 </script>
+
+<style>
+body {
+  font-family: 'Alata';
+  --bg-primary: #171725;
+  --fg-primary: #5e7c9b;
+  background: var(--bg-primary);
+  color: var(--fg-primary);
+  padding-right: 2rem;
+  padding-left: 2rem;
+  animation-delay: 2s;
+}
+
+.home {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+#nav {
+  padding-top: 1rem;
+  padding-bottom: 1rem;
+  margin: 0;
+  list-style: none;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+}
+
+#title {
+  font-size: 1.5rem;
+  color: rgb(89, 199, 199);
+}
+
+#nav a {
+  font-weight: bold;
+  text-decoration: none;
+  color: #2c3e50;
+}
+
+#nav a:hover {
+  color: aliceblue;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
